7622b18617cc767006de9e32281e104d9c44f1f6
gaye <gaye@mozilla.com>
2014-07-29 22:08:09 -0400

    Small cleanup

---
+++ test/calendars_test.js
@@ -0,0 +1,13 @@
+/* global calendars, chai */
+'use strict';
+
+var assert = chai.assert;
+
+describe('calendars', function() {
+  describe('#echo', function() {
+    it('should return arguments', function() {
+      return calendars.echo(4, 'b', 'c', 1, 2, 'e')
+      .should.eventually.deep.equal([4, 'b', 'c', 1, 2, 'e']);
+    });
+  });
+});
