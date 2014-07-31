8c5ecad751a234697922d9b0b9453c67959657ba
gaye <gaye@mozilla.com>
2014-07-31 19:24:53 -0400

    Fix lint tests

--- test/calendars_test.js
+++ test/calendars_test.js
@@ -1,13 +1,14 @@
 /* global calendars, chai */
 'use strict';
 
-var assert = chai.assert;
-
 describe('calendars', function() {
   describe('#echo', function() {
     it('should return arguments', function() {
       return calendars.echo(4, 'b', 'c', 1, 2, 'e')
-      .should.eventually.deep.equal([4, 'b', 'c', 1, 2, 'e']);
+      .should
+      .eventually
+      .deep
+      .equal([4, 'b', 'c', 1, 2, 'e']);
     });
   });
 });
